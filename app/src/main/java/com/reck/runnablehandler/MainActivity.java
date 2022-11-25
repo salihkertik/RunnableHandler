0e6b34d0e94583a1a1d4a175552bc262632fa091
salihkertik <salihkertik1453@gmail.com>
2022-11-26 01:18:39 +0300

    Initial commit

---
+++ app/src/main/java/com/reck/runnablehandler/MainActivity.java
@@ -0,0 +1,57 @@
+package com.reck.runnablehandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Runnable runnable;
+    int number;
+    Handler handler;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        number=0;
+    }
+
+    public void start (View view)
+    {
+        handler=new Handler(); // run metotumuzu ayarladıktan sonra handlerın içerisinden runnable'ı çağıracağımız için böyle bir nesne oluşturuyoruz.
+
+        runnable=new Runnable() //runnable metotu oluşturunca altta hazır override methodunu bize sağlıyor.
+        {
+            @Override
+            public void run()   // hazır gelen metotumuzun içine gereken bilgileri yazıyoruz.
+            {
+                textView.setText("Time: " + number);
+                number++;
+                textView.setText("Time: "+number);
+                handler.postDelayed(runnable, 1000); // runnable'ı her 1 saniye de bir çalıştır.
+            }
+        };
+
+        handler.post(runnable);   // handler'ın içerisinde runnable'ı çağırıp ele alıyoruz.
+        button.setEnabled(false); // start'a bastığımız andan itibaren butonu tıklanmaz yapıyoruz.
+    }
+
+    public void stop (View view)
+    {
+        button.setEnabled(true); // stop tuşuna bastıktan sonra start tuşunu aktif hale getiriyoruz.
+
+        handler.removeCallbacks(runnable); // runnable'ı kapat diyoruz.
+        number = 0;
+        textView.setText("Time: "+number); // Time'ı 0 a tekrar döndürüyoruz.
+    }
+}
